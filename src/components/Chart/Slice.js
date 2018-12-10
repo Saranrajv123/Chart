c18e30c337a0153d8772dd802f046aee2cda69fe
saranraj <vsaranraj16@gmail.com>
2018-12-11 00:15:28 +0530

    Label fix

--- src/components/Chart/Slice.js
+++ src/components/Chart/Slice.js
@@ -17,7 +17,7 @@ class Slice extends Component {
     }
 
     chartAnimate = () => {
-        this.drawChart(true);
+        this.drawChart(0);
 
     }
 
@@ -35,11 +35,14 @@ class Slice extends Component {
         return { x1, y1, x2, y2 };
       }
 
+      
     drawChart = (s) => {
+
         let path = [], a, b, c;
+
 		let step = this.props.angle / (37.5 / 2);
 
-		if (s + step > this.props.angle) {
+		if ( step > this.props.angle) {
 			s = this.props.angle;
 		}
 
@@ -54,22 +57,24 @@ class Slice extends Component {
 
 		path.push('Z');
 
-		this.setState({ path: path.join(' ') });
+        this.setState({ path: path.join(' ') });
+        
 
 		if (s < this.props.angle) {
 			setTimeout(() => { this.drawChart(s + step) } , 16);
 		} else if (this.props.showLabel) {
 			c = this.getAnglePoint(this.props.startAngle, this.props.startAngle + (this.props.angle / 2), (this.props.radius / 2 + this.props.trueHole / 2), this.props.radius);
-
+            console.log(c)
 			this.setState({
-				xAxis: c.x2,
-				yAxis: c.y2
-			});
+				xAxis: c.x1,
+				yAxis: c.x2
+            });
+            
 		}
 	}
     render() {
         return (
-            <g>
+            <g style={{ overflow: "hidden"}}>
           
             <path
                 d={ this.state.path }
@@ -77,7 +82,7 @@ class Slice extends Component {
                 stroke={ this.props.stroke }
                 strokeWidth={ this.props.strokeWidth ? this.props.strokeWidth : 3 }
                  />
-            { this.props.showLabel && this.props.percentValue > 10 ?
+            { this.props.showLabel && this.props.percentValue > 5 ?
                 <text x={ this.state.xAxis } y={ this.state.yAxis } fill="#fff" textAnchor="middle">
                     { this.props.percentage ? this.props.percentValue + '%' : this.props.value }
                 </text>
